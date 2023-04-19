3e4f3c051fd2b21a672573001e3f001bda7f8f8d
Splerger <jamiestewart38@icloud.com>
2023-04-19 21:57:15 +0000

    progress bro

--- main.ts
+++ main.ts
@@ -1,25 +1,29 @@
-basic.forever(function () {
-    checkbuttoninput()
-})
-
+// make starting barriers on right
 led.plot(4, 0)
 led.plot(4, 1)
 led.plot(4, 2)
 led.plot(4, 3)
 led.plot(4, 4)
 
+
+// make starting barriers on left
 led.plot(0, 0)
 led.plot(0, 1)
 led.plot(0, 2)
 led.plot(0, 3)
 led.plot(0, 4)
 
+// draw first object
 led.plot(2, 0)
 
 let posx = 2
 let posy = 0
 
-function checkbuttoninput() {
+basic.forever(function () {
+    main()
+})
+
+function main () {
     input.onButtonPressed(Button.A, function() {
         led.unplot(posx, posy)
         if (posx == 1) {
@@ -33,31 +37,41 @@ function checkbuttoninput() {
 
     input.onButtonPressed(Button.B, function () {
         led.unplot(posx, posy)
-        if (posx == 4) {
-            led.unplot(posx, posy)
-            led.plot(posx, posy)
-        } else {
-            posx++
-            led.plot(posx, posy)
-        }
+            if (posx == 3) {
+                led.unplot(posx, posy)
+                led.plot(posx, posy)
+            } else {
+                posx++
+                led.plot(posx, posy)
+            }
     })
 
-    control.waitMicros(3000000)
     switch (posy) {
-        case 4:
-            posy == 4
-            led.plot(posx, posy)
-            posx = 2
-            posy = 0
-            led.plot(posx, posy)
+        case 1:
+            
             break
-        
+
+        case 2:
+
+            break
+
+        case 3:
+
+            break
+
+        case 4:
+
+            break
     }
+    /* move every one second
+       Done in microsecond because stupid
+    */
+    control.waitMicros(1000000)
     led.unplot(posx, posy)
     if (posy == 4) {
-        
+    	
     } else {
-        posy++
+        posy += 1
     }
     led.plot(posx, posy)
 }
